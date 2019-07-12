8aecfd7db95e6ff25ae57ce0d77b19dae35a16e6
Rajat Roy <rroy1@illumina.com>
2019-07-12 16:33:53 -0700

    my work form before the break

--- StringsAreEvil/LineParser.cs
+++ StringsAreEvil/LineParser.cs
@@ -10,15 +10,52 @@
     public sealed class LineParser : ILineParser
     {
         public long TotalMileage;
-
+        private static ValueHolder _valueHolder = new ValueHolder();
         public void ParseLine(string line)
         {
-            var parts = line.Split(',');
-            if (parts[0] == "MNO")
+            int i = 0;
+            var intVal = 0;
+            (i, intVal) = GetNextInt(line, i);
+            (i, intVal) = GetNextInt(line, i+1);
+            var elementId = intVal;
+
+            (i, intVal) = GetNextInt(line, i+1);
+            var vehicleId = intVal;
+            (i, intVal) = GetNextInt(line, i+1);
+            var term = intVal;
+
+            (i, intVal) = GetNextInt(line, i+1);
+            var mileage = intVal;
+
+            (i, intVal) = GetNextInt(line, i+1);
+            var value = intVal;
+
+            //var parts = line.Split(',');
+            //var elementId = int.Parse(parts[1]); 
+            //var vehicleId = int.Parse(parts[2]);
+            //var term = int.Parse(parts[3]);
+            //var mileage = int.Parse(parts[4]);
+            //var value = decimal.Parse(parts[5]);
+
+
+            if (line[0] == 'M' && line[1]=='N' && line[2] == 'O')
             {
-                var temp = new ValueHolder(line);
+                _valueHolder.Set(elementId, vehicleId, term, mileage, value);
+                var temp = _valueHolder;
                 TotalMileage += temp.Mileage;
             }
+        }
+
+        private (int,int ) GetNextInt(string line, int i)
+        {
+            int num = 0;
+            while (line[i] != ',')
+            {
+                num *= 10;
+                num += (int)(line[i] - '0');
+                i++;
+            }
+            return (i, num);
         }
     }
 }
--- StringsAreEvil/ValueHolder.cs
+++ StringsAreEvil/ValueHolder.cs
@@ -1,22 +1,21 @@
 ﻿namespace StringsAreEvil
 {
-    public class ValueHolder
+    public  struct ValueHolder
     {
-        public int ElementId { get; }
-        public int VehicleId { get; }
-        public int Term { get; }
-        public int Mileage { get; }
-        public decimal Value { get; }
+        public  int ElementId ;
+        public  int VehicleId ;
+        public  int Term ;
+        public  int Mileage ;
+        public  decimal Value;
 
-        public ValueHolder(string line)
+        
+        public void Set(int elementId, int vehicleId, int term, int mileage, decimal value)
         {
-            var parts = line.Split(',');
-
-            ElementId = int.Parse(parts[1]);
-            VehicleId = int.Parse(parts[2]);
-            Term      = int.Parse(parts[3]);
-            Mileage   = int.Parse(parts[4]);
-            Value     = decimal.Parse(parts[5]);
+            ElementId = elementId; 
+            VehicleId = vehicleId;
+            Term = term;
+            Mileage   = mileage;
+            Value     = value;
         }
 
         public override string ToString() => ElementId + "," + VehicleId + "," + Term + "," + Mileage + "," + Value;
